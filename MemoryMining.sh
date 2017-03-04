f1a83a86e4c11acc1799f5c0ad28593c12d30133
root <faust.joshua@gmail.com>
2017-03-03 21:52:25 -0600

    Changed pathway to subscript directory in Mem script

---
+++ MemoryMining.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#==================================================
+#Mine primary memory segments for data
+#By: Joshua Faust
+#==================================================
+
+echo ========================
+echo Primary Memory Miner
+echo ========================
+echo 
+echo -------------------------------------------
+echo  Creating directory to store process dump 
+echo -------------------------------------------
+echo     All data gathered will be stored in
+echo      mining_out_data Directory unless
+echo	  specified otherwise by the user
+echo -------------------------------------------
+mkdir mining_output_data 
+cd mining_output_data
+ps -aux | awk '{print $1,$2,$11}' > all_processes.txt #Obtain all processes from all users
+ps -u $USER > user_processes.txt #Obtain user level processes
+echo 
+echo -n "Would you like to see current running Processes PID's?(y or n) "; read PID
+clear # clear the screen
+echo ------------------------------------------
+echo        Current Running Processes:
+echo ------------------------------------------
+echo
+if [ $PID == "y" ] || [ $PiD == "Y" ]
+	then
+	echo ------------------------
+	echo Showing All Processes:
+	echo ------------------------
+	cat all_processes.txt
+	echo ------------------------------
+	echo Showing User Level Processes:
+	echo ------------------------------
+	cat user_processes.txt
+	echo ----------------------------------------
+	echo
+	#Call the mining script:
+	/home/josh/Linux_Memory_Mining/subScripts/gdb_mine.sh
+	else
+	echo -----------------------------
+	echo Starting Mining Processes
+	echo -----------------------------
+	echo
+	#Call the mining script:
+	/home/josh/Linux_Memory_Mining/subScripts/gdb_mine.sh
+
+fi
+
+
---
+++ subScripts/gdb_mine.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#========================================
+#GDB processes: Gaining data
+#By: Joshua Faust
+#========================================
+
+
+echo ------------------------------
+echo Process ID Data Required:
+echo ------------------------------
+echo -n "What is the PID you are inquiring about: "; read PID
+awk '{print $1,$6}' /proc/$PID/maps > "${PID}_maps"
+clear #Clear data on screen
+echo ---------------------------
+echo Starting Mining Processes
+echo ---------------------------
+#
+# Ask user if you would like to view the memory map segments of the processes they chose.
+#
+echo -n "Would you like to see PID $PID memory map? (y or n) "; read pidAnswer
+if [ $pidAnswer == "y" ] || [ $pidAnswer == "Y" ] 
+	then
+	echo
+	cat "${PID}_maps"
+	else
+	echo continuing to GDB session...
+	echo
+fi
+#
+# Start of the GDB session:
+#
+echo -n "Are you ready to start a GDB session on $PID? (y or n) "; read mineAnswer
+if [ $mineAnswer == "y" ] || [ $mineAnswer == "Y" ]
+	then
+	echo --------------------------------------------------------------------------------
+	echo Getting Register Information
+	echo
+	echo To dump: dump [memory,binary,tekhex, verilog] FileLocation Memory Segment Range
+	echo To quit: quit
+	echo --------------------------------------------------------------------------------
+	echo
+	echo 
+	gdb --pid $PID -ex 'info registers' # to add another command -ex command
+	else
+	echo
+	echo closing program....
+fi
+rm -rf PID.txt #remove the temp file created to hold the PID
+
+
+
