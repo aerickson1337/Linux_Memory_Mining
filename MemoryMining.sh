27adf2fab61877a19c0222db1459d58f24d29550
root <faust.joshua@gmail.com>
2017-03-03 22:02:05 -0600

    Cleaned up some of the code

--- MemoryMining.sh
+++ MemoryMining.sh
@@ -5,17 +5,17 @@
 #By: Joshua Faust
 #==================================================
 
-echo ========================
-echo Primary Memory Miner
-echo ========================
+echo '============================================'
+echo '           Primary Memory Miner             '
+echo '============================================'
 echo 
-echo -------------------------------------------
-echo  Creating directory to store process dump 
-echo -------------------------------------------
-echo     All data gathered will be stored in
-echo      mining_out_data Directory unless
-echo	  specified otherwise by the user
-echo -------------------------------------------
+echo '-------------------------------------------'
+echo ' Creating directory to store process dump  '
+echo '-------------------------------------------'
+echo '    All data gathered will be stored in    '
+echo '     mining_out_data Directory unless      '
+echo '     specified otherwise by the user       '
+echo '-------------------------------------------'
 mkdir mining_output_data 
 cd mining_output_data
 ps -aux | awk '{print $1,$2,$11}' > all_processes.txt #Obtain all processes from all users
@@ -23,32 +23,32 @@ ps -u $USER > user_processes.txt #Obtain user level processes
 echo 
 echo -n "Would you like to see current running Processes PID's?(y or n) "; read PID
 clear # clear the screen
-echo ------------------------------------------
-echo        Current Running Processes:
-echo ------------------------------------------
+echo '------------------------------------------'
+echo '       Current Running Processes:         '
+echo '------------------------------------------'
 echo
 if [ $PID == "y" ] || [ $PiD == "Y" ]
 	then
-	echo ------------------------
-	echo Showing All Processes:
-	echo ------------------------
+	echo '-----------------------'
+	echo 'Showing All Processes: '
+	echo '-----------------------'
 	cat all_processes.txt
-	echo ------------------------------
-	echo Showing User Level Processes:
-	echo ------------------------------
+	echo '------------------------------'
+	echo 'Showing User Level Processes: '
+	echo '------------------------------'
 	cat user_processes.txt
-	echo ----------------------------------------
+	echo '----------------------------------------'
 	echo
 	#Call the mining script:
 	/home/josh/Linux_Memory_Mining/subScripts/gdb_mine.sh
 	else
-	echo -----------------------------
-	echo Starting Mining Processes
-	echo -----------------------------
+	echo '-----------------------------'
+	echo 'Starting Mining Processes    '
+	echo '-----------------------------'
 	echo
 	#Call the mining script:
 	/home/josh/Linux_Memory_Mining/subScripts/gdb_mine.sh
 
 fi
-
+exit 1
 
--- subScripts/gdb_mine.sh
+++ subScripts/gdb_mine.sh
@@ -5,15 +5,15 @@
 #========================================
 
 
-echo ------------------------------
-echo Process ID Data Required:
-echo ------------------------------
+echo '------------------------------'
+echo 'Process ID Data Required:     '
+echo '------------------------------'
 echo -n "What is the PID you are inquiring about: "; read PID
 awk '{print $1,$6}' /proc/$PID/maps > "${PID}_maps"
 clear #Clear data on screen
-echo ---------------------------
-echo Starting Mining Processes
-echo ---------------------------
+echo '---------------------------'
+echo 'Starting Mining Processes  '
+echo '---------------------------'
 #
 # Ask user if you would like to view the memory map segments of the processes they chose.
 #
@@ -23,7 +23,7 @@ if [ $pidAnswer == "y" ] || [ $pidAnswer == "Y" ]
 	echo
 	cat "${PID}_maps"
 	else
-	echo continuing to GDB session...
+	echo 'continuing to GDB session...'
 	echo
 fi
 #
@@ -32,18 +32,18 @@ fi
 echo -n "Are you ready to start a GDB session on $PID? (y or n) "; read mineAnswer
 if [ $mineAnswer == "y" ] || [ $mineAnswer == "Y" ]
 	then
-	echo --------------------------------------------------------------------------------
-	echo Getting Register Information
-	echo
-	echo To dump: dump [memory,binary,tekhex, verilog] FileLocation Memory Segment Range
-	echo To quit: quit
-	echo --------------------------------------------------------------------------------
+	echo '--------------------------------------------------------------------------------'
+	echo '                       Getting Register Information                             '
+	echo ''
+	echo 'To dump: dump [memory,binary,tekhex, verilog] FileLocation Memory Segment Range '
+	echo 'To quit: quit'
+	echo '--------------------------------------------------------------------------------'
 	echo
 	echo 
 	gdb --pid $PID -ex 'info registers' # to add another command -ex command
 	else
 	echo
-	echo closing program....
+	echo 'closing program....'
 fi
 rm -rf PID.txt #remove the temp file created to hold the PID
 
